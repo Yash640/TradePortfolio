2f8e5ef5df6627c41cf11512ae5add38cc1b13fe
Yash640 <110631002+Yash640@users.noreply.github.com>
2026-01-01 02:59:31 +0530

    feat: Add Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
 import './globals.css'
+import { Analytics } from '@vercel/analytics/react';
 
 const poppins = Poppins({ 
   subsets: ['latin'],
@@ -20,7 +21,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${poppins.variable} ${poppins.className}`}>{children}</body>
+      <body className={`${poppins.variable} ${poppins.className}`}>{children}<Analytics /></body>
     </html>
   )
 }
